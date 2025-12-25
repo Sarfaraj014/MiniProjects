56940f69e94fc8ac289e045d3996cdd449196d8c
Sarfaraj014 <sarfarajhussain692@gmail.com>
2025-12-25 20:46:32 +0530

    writing code

---
+++ rentcode.py
@@ -0,0 +1,14 @@
+#taking input by the user 
+Rent=int(input("Enter your total Rent : "))
+spendUnit=int(input("Total Electricity units spend : "))
+unitcharge=int(input("Electriciy charge per unit : "))
+personNo=int(input("Number of person to devide the amount : "))
+
+#perform substraction,multipication and divison
+
+electricityExpences=unitcharge*spendUnit
+totalAmount=(Rent + electricityExpences)/personNo
+
+#how we will print the final output
+
+print(f"Each will pay = {str(totalAmount)}")
